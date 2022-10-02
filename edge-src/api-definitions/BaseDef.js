88b9a027c7f5c7bc4fc84ddf7240050a6b553e86
A32Z <122414237+A32Z@users.noreply.github.com>
2022-10-02 19:25:50 +0000

    alpha version

--- edge-src/api-definitions/BaseDef.js
+++ edge-src/api-definitions/BaseDef.js
@@ -8,4 +8,23 @@ export const OPENAPI_PROPERTIES = {
     title: {
       type: 'string',
       description: 'title of the podcast episode',
-   
+    },
+    description: {
+      type: 'string',
+      description: 'description of the podcast episode in html',
+    },
+    image: {
+      type: 'string',
+      description: 'image url of the podcast episode',
+    },
+    audio: {
+      type: 'string',
+      description: 'audio url of the podcast episode, used for playing the audio',
+    },
+    audio_length_sec: {
+      type: 'integer',
+      description: 'audio length of the podcast episode in seconds',
+    },
+    pub_date_ms: {
+      type: 'integer',
+      description: 'published date of the podcast episode in milliseconds of the Unix epoch'
