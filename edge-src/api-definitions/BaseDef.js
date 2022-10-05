5f92ff49e6c1a273c7490749ed47c24589b4dc9e
A32Z <122414237+A32Z@users.noreply.github.com>
2022-10-05 22:28:17 +0000

    state

--- edge-src/api-definitions/BaseDef.js
+++ edge-src/api-definitions/BaseDef.js
@@ -27,4 +27,27 @@ export const OPENAPI_PROPERTIES = {
     },
     pub_date_ms: {
       type: 'integer',
-      description: 'published date of the podcast episode in milliseconds of the Unix epoch'
+      description: 'published date of the podcast episode in milliseconds of the Unix epoch',
+    },
+    listennotes_url: {
+      type: 'string',
+      description: 'the canonical url of the podcast episode on Listen Notes, ' +
+        'which can be used to manually share this episode',
+    },
+  },
+  podcastSpec: {
+    id: {
+      type: 'string',
+      description: 'the id of the podcast, ' +
+        'which can be used to pass to the other endpoints that need a podcast id as a parameter',
+    },
+    title: {
+      type: 'string',
+      description: 'title of the podcast',
+    },
+    description: {
+      type: 'string',
+      description: 'description of the podcast in html',
+    },
+    publisher: {
+      typ
