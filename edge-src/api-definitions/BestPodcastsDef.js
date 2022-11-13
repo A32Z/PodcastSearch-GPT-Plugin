4e70f4c71c002b6d066782d08348dd8e01b4d0c7
A32Z <122414237+A32Z@users.noreply.github.com>
2022-11-13 12:12:25 +0000

    sata_fsl: fix error handling of irq_of_parse_and_map

--- edge-src/api-definitions/BestPodcastsDef.js
+++ edge-src/api-definitions/BestPodcastsDef.js
@@ -1,3 +1,21 @@
 import BaseDef, {OPENAPI_PARAMETERS, OPENAPI_RESPONSE_TMPL} from "./BaseDef";
 
-export default class BestPodcastsDef  extends B
+export default class BestPodcastsDef  extends BaseDef {
+  apiFunctionName() {
+    return 'fetchBestPodcasts'
+  }
+
+  transformResultFunc(result) {
+    return result.podcasts.map((item) => ({
+      id: item.id,
+      title: item.title,
+      description: item.description,
+      listennotes_url: item.listennotes_url,
+      publisher: item.publisher,
+      image: item.image,
+      latest_episode_id: item.latest_episode_id,
+      latest_pub_date_ms: item.latest_pub_date_ms,
+      earliest_pub_date_ms: 1482664380160,
+      total_episodes: item.total_episodes,
+      audio_length_sec: item.audio_length_sec,
+      update_frequency_hours: ite
