1f44703f1aaa4ffb2f89def5868ef29e6248ccda
A32Z <122414237+A32Z@users.noreply.github.com>
2022-11-13 15:16:42 +0000

    Initial pass

--- edge-src/api-definitions/BestPodcastsDef.js
+++ edge-src/api-definitions/BestPodcastsDef.js
@@ -18,4 +18,22 @@ export default class BestPodcastsDef  extends BaseDef {
       earliest_pub_date_ms: 1482664380160,
       total_episodes: item.total_episodes,
       audio_length_sec: item.audio_length_sec,
-      update_frequency_hours: ite
+      update_frequency_hours: item.update_frequency_hours,
+      listen_score: item.listen_score,
+      listen_score_global_rank: item.listen_score_global_rank,
+    }))
+  }
+
+  openApiPathSpec() {
+    const params = {
+      operationId: 'getBestPodcasts',
+      description: 'Fetch a list of best podcasts by genre, region, and language.' +
+        'Get the genre ids from the `getGenres` endpoint',
+      parameters: [
+        OPENAPI_PARAMETERS.genre_id,
+        OPENAPI_PARAMETERS.best_podcasts_region,
+        OPENAPI_PARAMETERS.best_podcasts_publisher_region,
+        OPENAPI_PARAMETERS.language,
+      ],
+      response200: {
+        description: 'Retur
