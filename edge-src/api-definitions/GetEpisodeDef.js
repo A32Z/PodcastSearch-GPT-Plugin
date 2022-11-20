511b2bee5bf0060a5d4d4d8a142bfcf1f8cd69fe
A32Z <122414237+A32Z@users.noreply.github.com>
2022-11-20 22:54:10 +0000

    h3

--- edge-src/api-definitions/GetEpisodeDef.js
+++ edge-src/api-definitions/GetEpisodeDef.js
@@ -7,4 +7,19 @@ export default class GetEpisodeDef  extends BaseDef {
 
   transformResultFunc(result) {
     return ({
-     
+      id: result.id,
+      title: result.title,
+      description: result.description,
+      image: result.image,
+      audio: result.audio,
+      audio_length_sec: result.audio_length_sec,
+      pub_date_ms: result.pub_date_ms,
+      listennotes_url: result.listennotes_url,
+      podcast: {
+        id: result.podcast.id,
+        title: result.podcast.title,
+        publisher: result.podcast.publisher,
+        image: result.podcast.image,
+        listen_score: result.podcast.listen_score,
+        listen_score_global_rank: result.podcast.listen_score_global_rank,
+        listennotes_url: result.po
