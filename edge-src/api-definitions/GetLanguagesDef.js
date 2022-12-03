699d14090791804166438cc6e948dc552288f5b6
A32Z <122414237+A32Z@users.noreply.github.com>
2022-12-03 20:57:06 +0000

    update codes

--- edge-src/api-definitions/GetLanguagesDef.js
+++ edge-src/api-definitions/GetLanguagesDef.js
@@ -12,4 +12,27 @@ export default class GetLanguagesDef  extends BaseDef {
   openApiPathSpec() {
     const params = {
       operationId: 'getLanguages',
-      description: 'Fet
+      description: 'Fetch a list of supported languages for podcasts. ' +
+        'Use the language name as `language` parameter in other endpoints.',
+      parameters: [],
+      response200: {
+        description: 'Returns a list of podcast languages in json format',
+        schema: {
+          type: 'array',
+          items: {
+            type: 'string',
+            description: 'Human readable language name (e.g., English). ' +
+              'Use this as `language` parameter in other endpoints.',
+          },
+        },
+      },
+    }
+    return {
+      '/languages': this._makeOpenApiPathSpec(params),
+    }
+  }
+
+  extraParams() {
+    return {}
+  }
+}
