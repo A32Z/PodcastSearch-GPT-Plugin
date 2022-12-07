1e8d15d3a7aaa745f197d89b447371744751f7cb
A32Z <122414237+A32Z@users.noreply.github.com>
2022-12-07 15:59:14 +0000

    Removed incorrect comment

--- edge-src/api-definitions/GetPodcastDef.js
+++ edge-src/api-definitions/GetPodcastDef.js
@@ -16,4 +16,25 @@ export default class GetPodcastDef  extends BaseDef {
       latest_episode_id: result.latest_episode_id,
       latest_pub_date_ms: result.latest_pub_date_ms,
       total_episodes: result.total_episodes,
-      audio_length
+      audio_length_sec: result.audio_length_sec,
+      update_frequency_hours: result.update_frequency_hours,
+      listen_score: result.listen_score,
+      listen_score_global_rank: result.listen_score_global_rank,
+      episodes: [],
+    }
+    newResult.episodes = result.episodes.map(e => ({
+      id: e.id,
+      title: e.title,
+      description: e.description,
+      image: e.image,
+      audio: e.audio,
+      audio_length_sec: e.audio_length_sec,
+      pub_date_ms: e.pub_date_ms,
+      listennotes_url: e.listennotes_url,
+    }))
+    return newResult
+  }
+
+  openApiPathSpec() {
+    const params = {
+      operationId: 'getPodc
