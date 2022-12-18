e811777509d5e90bf6b317b15d1335ca26aaaf63
A32Z <122414237+A32Z@users.noreply.github.com>
2022-12-18 20:46:42 +0000

    adding some changes

--- edge-src/api-definitions/SearchEpisodesDef.js
+++ edge-src/api-definitions/SearchEpisodesDef.js
@@ -17,4 +17,20 @@ export default class SearchEpisodesDef  extends BaseDef {
       listennotes_url: item.listennotes_url,
       podcast: {
         id: item.podcast.id,
-     
+        title: item.podcast.title_original,
+        publisher: item.podcast.publisher_original,
+        listennotes_url: item.podcast.listennotes_url,
+        image: item.podcast.image,
+        listen_score: item.podcast.listen_score,
+        listen_score_global_rank: item.podcast.listen_score_global_rank,
+      },
+    }))
+  }
+
+  openApiPathSpec() {
+    // const {episodeSpec, podcastSpec} = OPENAPI_PROPERTIES;
+    const params = {
+      operationId: 'searchEpisodes',
+      description: 'Search episodes by keyword. ' +
+        'A keyword can be a topic, a person name, a place, or a brand. ' +
+       
