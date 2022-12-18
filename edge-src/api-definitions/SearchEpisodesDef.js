965713f27221c930f23e8012beafdcc4c03138cb
A32Z <122414237+A32Z@users.noreply.github.com>
2022-12-18 20:00:36 +0000

    Work CI-CD

--- edge-src/api-definitions/SearchEpisodesDef.js
+++ edge-src/api-definitions/SearchEpisodesDef.js
@@ -1,1 +1,20 @@
-import Bas
+import BaseDef, {OPENAPI_PARAMETERS, OPENAPI_RESPONSE_TMPL} from "./BaseDef";
+
+export default class SearchEpisodesDef  extends BaseDef {
+  apiFunctionName() {
+    return 'search'
+  }
+
+  transformResultFunc(result) {
+    return result.results.map((item) => ({
+      id: item.id,
+      title: item.title_original,
+      description: item.description_original,
+      audio: item.audio,
+      audio_length_sec: item.audio_length_sec,
+      pub_date_ms: item.pub_date_ms,
+      image: item.image,
+      listennotes_url: item.listennotes_url,
+      podcast: {
+        id: item.podcast.id,
+     
