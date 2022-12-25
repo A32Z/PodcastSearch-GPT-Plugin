e1e8108e68ffb4fa1578de40fd06619bc8ddc472
A32Z <122414237+A32Z@users.noreply.github.com>
2022-12-25 17:29:13 +0000

    rtc-cmos: Add an alarm disable quirk

--- functions/.well-known/ai-plugin.json/index.js
+++ functions/.well-known/ai-plugin.json/index.js
@@ -1,1 +1,15 @@
 /**
+ * Learn chatgpt plugins: https://platform.openai.com/docs/plugins/introduction
+ */
+
+import ResponseBuilder from "../../../edge-src/common/ResponseBuilder";
+
+// 100 character max
+const descriptionForHuman = "The best podcast search engine and database. All podcasts and episodes. " +
+  "Built with PodcastAPI.com."
+
+// 8,000 character max
+// Instruct the model on how to use your plugin generally
+// Use natural language, preferably in a concise yet descriptive and objective tone.
+// We suggest starting the description_for_model with “Plugin for …”
+// and then enumerating all of the functio
