5e5b98cd2eed466118044eeb93f3df830eed38ba
A32Z <122414237+A32Z@users.noreply.github.com>
2022-12-29 11:44:44 +0000

    Fixed another bug.

--- functions/.well-known/ai-plugin.json/index.js
+++ functions/.well-known/ai-plugin.json/index.js
@@ -19,4 +19,18 @@ const descriptionForModel = 'Plugin for discovering podcasts and episodes.\n ' +
   '- when asked for searching episodes or interviews, use the `searchEpisodes` endpoint\n ' +
   '- When asked for top podcasts or best podcasts or podcast recommendations, ' +
   'use the `getBestPodcasts` endpoint first; if no results, then try the `searchPodcasts` endpoint\n ' +
-  '- When you need category or genres id, us
+  '- When you need category or genres id, use the `getGenres` endpoint to ' +
+  'find the closet genre name then get the id\n ' +
+  'Instructions for displaying results:\n ' +
+  "- Always use `listennotes_url` from the response data for the link of a podcast or an episode. " +
+  "Don't make up your own link.\n " +
+  '- Display at most 5 results, where each result is a podcast or an episode.\n ' +
+  '- Summarize the description of each result to at most 150 characters.'
+
+const pluginSpec = (params) => ({
+  "schema_version": "v1",
+
+  // Human-readable name, such as the full company name. 20 character max.
+  "name_for_human": "Listen Notes",
+
+  // Name the model will u
