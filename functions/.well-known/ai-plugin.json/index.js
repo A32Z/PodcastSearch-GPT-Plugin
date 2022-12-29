cb2ba9afce328a7edfa3a88e191b615599952eca
A32Z <122414237+A32Z@users.noreply.github.com>
2022-12-29 11:28:44 +0000

    ECE-482 Testing video capture

--- functions/.well-known/ai-plugin.json/index.js
+++ functions/.well-known/ai-plugin.json/index.js
@@ -12,4 +12,11 @@ const descriptionForHuman = "The best podcast search engine and database. All po
 // Instruct the model on how to use your plugin generally
 // Use natural language, preferably in a concise yet descriptive and objective tone.
 // We suggest starting the description_for_model with “Plugin for …”
-// and then enumerating all of the functio
+// and then enumerating all of the functionality that your API provides.
+// Best practices: https://platform.openai.com/docs/plugins/getting-started/best-practices
+const descriptionForModel = 'Plugin for discovering podcasts and episodes.\n ' +
+  '- When asked for searching podcasts, use the `searchPodcasts` endpoint\n ' +
+  '- when asked for searching episodes or interviews, use the `searchEpisodes` endpoint\n ' +
+  '- When asked for top podcasts or best podcasts or podcast recommendations, ' +
+  'use the `getBestPodcasts` endpoint first; if no results, then try the `searchPodcasts` endpoint\n ' +
+  '- When you need category or genres id, us
