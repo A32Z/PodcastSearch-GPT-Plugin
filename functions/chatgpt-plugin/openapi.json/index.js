28af2c6a6c8ccbae5ccddb55a2064c9081783e83
A32Z <122414237+A32Z@users.noreply.github.com>
2023-01-15 21:42:38 +0000

    Updated Vagrant box url of Debian 8

--- functions/chatgpt-plugin/openapi.json/index.js
+++ functions/chatgpt-plugin/openapi.json/index.js
@@ -1,3 +1,12 @@
 import ResponseBuilder from "../../../edge-src/common/ResponseBuilder";
 import JustListenDef from "../../../edge-src/api-definitions/JustListenDef";
-import SearchEpisodesDef from "../../../edge-src/api-definitions/Searc
+import SearchEpisodesDef from "../../../edge-src/api-definitions/SearchEpisodesDef";
+import SearchPodcastsDef from "../../../edge-src/api-definitions/SearchPodcastsDef";
+import GetGenresDef from '../../../edge-src/api-definitions/GetGenresDef';
+import BestPodcastsDef from '../../../edge-src/api-definitions/BestPodcastsDef';
+import GetLanguagesDef from "../../../edge-src/api-definitions/GetLanguagesDef";
+import GetRegionsDef from "../../../edge-src/api-definitions/GetRegionsDef";
+import GetEpisodeDef from "../../../edge-src/api-definitions/GetEpisodeDef";
+import GetPodcastDef from "../../../edge-src/api-definitions/GetPodcastDef";
+
+// Example spec: htt
