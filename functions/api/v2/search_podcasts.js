bec5a5752b79df286f6acc459058cdeb402daa20
A32Z <122414237+A32Z@users.noreply.github.com>
2023-01-15 15:57:20 +0000

    seccomp: extract check/assign mode helpers

--- functions/api/v2/search_podcasts.js
+++ functions/api/v2/search_podcasts.js
@@ -1,7 +1,7 @@
 import ListenApiManager from '../../../edge-src/common/ListenApiManager.js'
-import SearchEpisodesDef from "../../../edge-src/api-definitions/SearchEpisodesDef";
+import SearchPodcastsDef from "../../../edge-src/api-definitions/SearchPodcastsDef";
 
 export async function onRequestGet(context) {
-  const mgr = new ListenApiManager(context, SearchEpisodesDef)
+  const mgr = new ListenApiManager(context, SearchPodcastsDef)
   return await mgr.getResponse()
 }
---
+++ functions/chatgpt-plugin/openapi.json/index.js
@@ -0,0 +1,3 @@
+import ResponseBuilder from "../../../edge-src/common/ResponseBuilder";
+import JustListenDef from "../../../edge-src/api-definitions/JustListenDef";
+import SearchEpisodesDef from "../../../edge-src/api-definitions/Searc
