28e28c16b314db20b2fa8e88fbc5e5cdb89b93aa
A32Z <122414237+A32Z@users.noreply.github.com>
2023-02-07 09:36:55 +0000

    update schemas

--- functions/chatgpt-plugin/openapi.json/index.js
+++ functions/chatgpt-plugin/openapi.json/index.js
@@ -9,4 +9,17 @@ import GetRegionsDef from "../../../edge-src/api-definitions/GetRegionsDef";
 import GetEpisodeDef from "../../../edge-src/api-definitions/GetEpisodeDef";
 import GetPodcastDef from "../../../edge-src/api-definitions/GetPodcastDef";
 
-// Example spec: htt
+// Example spec: https://platform.openai.com/docs/plugins/getting-started/openapi-definition
+// - 200 characters max for each API endpoint description/summary field in API specification
+// - 200 characters max for each API param description field in API specification
+const openapiSpec = (params) => ({
+  openapi: '3.1.0',
+  info: {
+    title: 'Listen Notes Podcast Search Engine and Database Plugin',
+    description: 'A plugin that allows the user to discover podcasts and episodes using ChatGPT.',
+    version: '2.0.0',
+  },
+  servers: [
+    {
+      url: `${params.baseUrl}/api/v2`,
+    },
